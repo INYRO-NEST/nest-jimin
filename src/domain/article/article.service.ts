41a6ae6b49f3d92a47f30523fdbfc877a906b1b9
Michael <7471919@naver.com>
2023-05-17 15:04:12 +0900

    feat: Read Article

--- src/domain/article/article.service.ts
+++ src/domain/article/article.service.ts
@@ -10,12 +10,25 @@ export class ArticleService {
     private readonly articleRepository: Repository<ArticleEntity>,
   ) {}
 
-  // 게시글 만드는 함수
+  // 게시글 만드는 서비스
   async createArticle(title: string, content: string, userId: string) {
     const article = await this.articleRepository.save({
       content: content,
       title: title,
       userId: userId,
+    });
+
+    return article;
+  }
+
+  async getArticle(articleId: string) {
+    const article = await this.articleRepository.findOne({
+      where: {
+        id: articleId,
+      },
+      relations: {
+        comments: true,
+      },
     });
 
     return article;
