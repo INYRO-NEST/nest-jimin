59220646ecb816582850fc47d38a140423270c15
Michael <7471919@naver.com>
2023-05-17 15:43:28 +0900

    UserController url user is chaneged users

--- src/domain/user/user.controller.ts
+++ src/domain/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
